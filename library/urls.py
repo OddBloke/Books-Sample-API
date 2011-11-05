d81fae26e97c4bb18894a725a6892d510cf79775
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-11-05 09:36:20 +0000

    Serve up our books API.

--- library/urls.py
+++ library/urls.py
@@ -1,4 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from piston.resource import Resource
+
+from books.handlers import BookHandler
+
+book_resource = Resource(BookHandler)
 
 urlpatterns = patterns('',
+    (r'^book/(?P<id>[^/.])', book_resource)
 )
